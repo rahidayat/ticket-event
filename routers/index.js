98a618cf004460cb13b8955c3952d4fa231551f5
rahid <hidayat.85@gmail.com>
2017-07-19 20:36:58 +0700

    edit router index utk add event, edit view index  nambah link add, nambah view add-event

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const model = require ('../models');
 
 router.get('/', (req,res)=> {
   res.render('index', {
@@ -7,6 +8,16 @@ router.get('/', (req,res)=> {
   })
 })
 
+router.get('/add-event', (req, res) => { // by dayat
+  res.render('add-event', {
+    title: 'Add Event'
+  })
+})
+
+router.post('/add-event', (req,res) => { // by dayat
+  model.Event.create(req.body)
+  .then(row => {res.redirect('/')})
+})
 
 
 
